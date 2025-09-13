bfa6028c63f5ec164cbf7aca4a8b2aaae451fa93
rene-vortmeier <rene.vortmeier@googlemail.com>
2025-09-13 21:15:23 +0200

    002: Ch2 L3

--- main.py
+++ main.py
@@ -6,13 +6,20 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     while True == True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        clock_time = clock.tick(60)
+        dt = clock_time/1000
 
 
 if __name__ == "__main__":
